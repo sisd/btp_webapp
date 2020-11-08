a3ad22cd2c1b06d399329493cfd46af793e9e7cb
sisd <32328883+sisd@users.noreply.github.com>
2020-11-08 19:51:03 +0530

    Update btp_flask.py

--- btp_flask.py
+++ btp_flask.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,6 +10,12 @@ def main():
 @app.route('/station')
 def hello():
     return 'Dropdown shit'
+
+@app.route('/station1', methods=['GET'])
+def dropdown():
+    states = ['s1', 's2', 's3', 's4']
+    stations = ['st1', 'st2', 'st3']
+    return render_template('test.html', states=states, stations=stations)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080)
